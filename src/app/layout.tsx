a54043b76ac6867dd256d56d03673566c181adfc
Marcus Skowronek <ms@redlark.de>
2024-07-14 21:04:11 +0200

    Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,10 +18,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="w-full bg-white md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+        <div className="w-full px-4 bg-white md:px-8 lg:px-16 xl:px-32 2xl:px-64">
           <Navbar />
         </div>
+        <div className="bg-slate-100 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         {children}
+        </div>
       </body>
     </html>
   );
