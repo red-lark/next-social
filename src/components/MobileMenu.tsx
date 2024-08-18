49334a1de531d1b20c498c0b3131c3ca8e2c0b69
Marcus Skowronek <ms@redlark.de>
2024-08-18 18:01:45 +0200

    Menu

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -4,13 +4,25 @@ import { useState } from "react";
 const MobileMenu = () => {
     const [open, setIsOpen] = useState(false);
     return (
-        <div className="">
+        <div className="md:hidden">
             
             <div className="flex flex-col cursor-pointer gap-[4.5]" onClick={() => setIsOpen((prev) => !prev)}>
-                <div className="w-6 h-1 bg-blue-500 rounded-sm"></div>
-                <div className="w-6 h-1 bg-blue-500 rounded-sm"></div>
-                <div className="w-6 h-1 bg-blue-500 rounded-sm"></div>
+                <div className={`w-6 h-1 bg-blue-500 rounded-sm ${open ? 'rotate-45' : ''} origin-left ease-in-out duration-500`}></div>
+                <div className={`w-6 h-1 bg-blue-500 rounded-sm ${open ? 'opacity-0' : ''} ease-in-out duration-500`}></div>
+                <div className={`w-6 h-1 bg-blue-500 rounded-sm ${open ? '-rotate-45' : ''} origin-left ease-in-out duration-500`}></div>
             </div>
+            {open && (
+                <div className="absolute left-0 top-24 w-full h-[calc(100vh-96px)] bg-white flex flex-col items-center justify-center gap-8 font-medium text-xl">
+                    <div className="flex flex-col gap-4 p-4">
+                        <a href="#" className="text-blue-600">Home</a>
+                        <a href="#" className="text-blue-600">Friends</a>
+                        <a href="#" className="text-blue-600">Groups</a>
+                        <a href="#" className="text-blue-600">Stories</a>
+                        <a href="#" className="text-blue-600">Login</a>
+
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,34 @@
 import Link from "next/link"
 import MobileMenu from "@/components/MobileMenu"
+import Image from "next/image"
 
 const Navbar = () => {
   return (
     <div className="h-24 flex items-center justify-between">
-      <div className="">
-        <Link href="/" className="font-bold text-xl text-blue-600">LamaSocial</Link>
+      <div className="md:hidden lg:block w-[20%] text-sm">
+        <Link href="/" className="font-bold text-xl text-blue-600">MEMORIA</Link>
       </div>
-      <div className="hidden">Navbar</div>
-      <div className="">
+      <div className="hidden md:flex w-[50%]">
+        <div className="flex gap-6 text-gray-600 mr-4">  
+          <Link href="/" className="flex items-center gap-2">
+            <Image src="/home.png" alt="home" width={16} height={16} className="w-4 h-4" />
+            <span>Homepage</span>
+          </Link>
+        </div>
+        <div className="flex gap-6 text-gray-600 mr-4">  
+          <Link href="/" className="flex items-center gap-2">
+            <Image src="/friends.png" alt="home" width={16} height={16} className="w-4 h-4" />
+            <span>Friends</span>
+          </Link>
+        </div>
+        <div className="flex gap-6 text-gray-600 mr-4">  
+          <Link href="/" className="flex items-center gap-2">
+            <Image src="/stories.png" alt="home" width={16} height={16} className="w-4 h-4"/>
+            <span>Stories</span>
+          </Link>
+        </div>
+      </div>
+      <div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end">
         <MobileMenu />
       </div>
     </div>
